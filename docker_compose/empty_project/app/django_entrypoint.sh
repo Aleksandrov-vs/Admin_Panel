3e8142432e017d4fbcfb9481765a21a56d612009
vasya <dfcz200115@mail.ru>
2023-03-28 23:54:53 +0600

    clear empty project dir

--- docker_compose/empty_project/app/django_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python manage.py migrate
-echo "from django.contrib.auth.models import User" > createadmin.py
-echo "User.objects.create_superuser('vasya', 'vasya@gmail.ru', 'vasya')" >> createadmin.py
-python manage.py shell < createadmin.py
-rm createadmin.py
-uwsgi --strict --ini uwsgi.ini
-exec "$@"
